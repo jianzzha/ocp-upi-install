ce202c2921423a5f046167a62bea2c1543d7f852
jianzhu zhang <jianzzha@redhat.com>
2020-06-13 16:12:38 +0000

    added cleanup.sh

---
+++ cleanup.sh
@@ -0,0 +1,28 @@
+#/usr/bin/bash
+set -euo pipefail
+
+./delete-vm.sh
+
+container_runtime=$(yq -r '.container_runtime' setup.conf.yaml)
+
+echo "stop container haproxy dnsmasq httpd services"
+
+for c in ocp-haproxy ocp-httpd ocp-dnsmasq; do
+    ${container_runtime} stop $c || true
+    ${container_runtime} rm $c || true
+done
+
+echo "stop bastion haproxy dnsmasq httpd services"
+for s in haproxy dnsmasq httpd; do
+    sudo systemctl disable --now $s || true
+done
+
+echo "clean up working directory"
+/bin/rm -rf pxelinux.cfg
+/bin/rm -rf dnsmasq
+/bin/rm -rf *.ign
+/bin/rm -rf www
+/bin/rm -rf fix-ign-*
+
+ 
+
