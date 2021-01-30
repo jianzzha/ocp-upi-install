321fe97a753b0fa85b75e4eb8c197b80e356c41c
Jianzhu Zhang <jianzzha@redhat.com>
2021-01-30 00:14:55 +0000

    added badfish local build

--- setup.sh
+++ setup.sh
@@ -13,6 +13,16 @@ function detect_os {
     source /etc/os-release
 }
 
+function build_badfish_image {
+    pushd ${SCRIPTPATH}
+    git clone https://github.com/redhat-performance/badfish.git
+    cp -f alias_idrac_interfaces.yml badfishi/config/idrac_interfaces.yml
+    cd badfish
+    podman build -t quay.io/jianzzha/alias .
+    popd
+}
+
+
 function add_pxe_files {
     mkdir -p tmp_syslinux
     sudo mkdir -p /var/lib/tftpboot
@@ -473,6 +483,14 @@ echo "start master ..."
 vmcount=0
 lab_name=$(yq -r '.lab_name' setup.conf.yaml)
 lab_name=${lab_name:-""}
+
+# if badfish image not exists, then build it with local change
+if [[ "${lab_name}" == "alias" ]]; then
+    if ! podman image pull quay.io/jianzzha/alias; then
+        build_badfish_image
+    fi
+fi
+
 masters=$(yq -r '.master | length' setup.conf.yaml)
 for i in $(seq 0 $((masters-1))); do
     type=$(yq -r .master[$i].type setup.conf.yaml)
